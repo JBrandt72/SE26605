f54d6f5990dfea1035839b2e43b78886424b60e4
Jon Brandt <jbrandt@email.neit.edu>
2017-10-16 08:21:18 -0400

    basic template

---
+++ wk3demo/assets/footer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 005501496
+ * Date: 10/16/2017
+ * Time: 8:14 AM
+ */
+?>
+
+</section>
+<footer>
+    Copyright &copy; <?php echo Date('Y'); ?> Jonathan Brandt. All rights reserved
+</footer>
+</body>
+</html>
---
+++ wk3demo/assets/header.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 005501496
+ * Date: 10/16/2017
+ * Time: 8:12 AM
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <!-- css links go here -->
+    <!-- js links go here -->
+</head>
+<body>
+    <header>
+        <nav></nav>
+    </header>
+    <section>
