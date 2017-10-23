62d55cffcc37e1791bb1fe7f3e2d6d0eeb718006
Jon Brandt <jbrandt@email.neit.edu>
2017-10-23 07:40:33 -0400

    lab3 starting point

---
+++ lab3/assets/actorform.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 005501496
+ * Date: 10/18/2017
+ * Time: 10:23 AM
+ */
+?>
+
+<form method="post" action="#">
+    First Name: <input type="text" name="firstname" value="" /><br /> <!--text boxes for user to enter data -->
+    Last Name: <input type="text" name="lastname" value="" /><br />
+    DOB: <input type="text" name="dob" value="" /><br />
+    Height: <input type="text" name="height" value="" /><br />
+
+
+    <input type="submit" id="foo" name="action" value="Add"> <!--submit button to add new record -->
+</form>
