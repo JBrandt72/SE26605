1158109210d900caf7fcaefd6cf7611038c91cf0
Jon Brandt <jbrandt@email.neit.edu>
2017-11-29 10:53:55 -0500

    foundation 1.0

---
+++ shoppingcart/assets/footer.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 005501496
+ * Date: 11/29/2017
+ * Time: 10:50 AM
+ */
+
+?>
+
+</section>
+</body>
+
+<footer style="clear:both">
+    Copyright &copy; <?php echo Date('Y'); ?> Jonathan Brandt. All rights reserved
+</footer>
+
+</html>
---
+++ shoppingcart/assets/header.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 005501496
+ * Date: 11/29/2017
+ * Time: 10:49 AM
+ */
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Shopping cart</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <!-- css links go here -->
+    <!-- js links go here -->
+</head>
+<body>
+<header>
+    <h2>URL Collector</h2>
+    <a href="index.php">Add</a>
+    <a href="search.php">Search</a>
+</header>
+<section>
